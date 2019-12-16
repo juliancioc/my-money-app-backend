7c155f5c13a3246311ccf58bcd1c5cddc8efe3e1
Juliancio <juliancio_alves@hotmail.com>
2019-12-16 01:57:56 -0300

    cors

--- src/config/server.js
+++ src/config/server.js
@@ -5,14 +5,16 @@ const express = require('express')
 const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
+let cors = require('cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
+server.use(cors())
 
 server.listen(process.env.PORT || port, function() {
-    console.log(`BACKEND is running on port ${port}.`)
+    console.log(`BACKEND is running on port ${process.env.PORT || port}.`)
 }) 
 
 module.exports = server
